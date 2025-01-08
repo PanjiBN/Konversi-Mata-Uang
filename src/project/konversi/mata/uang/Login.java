e5f69c4ba4254e7de75ee85d2934106802b718cd
DrunkenTapir367 <nabilvidiputra@gmail.com>
2025-01-08 21:55:28 +0700

    prototype5

--- src/project/konversi/mata/uang/Login.java
+++ src/project/konversi/mata/uang/Login.java
@@ -29,8 +29,12 @@ public class Login extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         btnLogin = new javax.swing.JButton();
         lblWelc = new javax.swing.JLabel();
+        lblWelc1 = new javax.swing.JLabel();
+        lblWelc2 = new javax.swing.JLabel();
+        lblWelc3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
 
         jPanel1.setBackground(new java.awt.Color(51, 51, 51));
 
@@ -41,28 +45,59 @@ public class Login extends javax.swing.JFrame {
             }
         });
 
+        lblWelc.setFont(new java.awt.Font("Arial Black", 1, 14)); // NOI18N
         lblWelc.setForeground(new java.awt.Color(255, 255, 255));
-        lblWelc.setText("Selamat Datang");
+        lblWelc.setText("Currency Exchange");
+
+        lblWelc1.setForeground(new java.awt.Color(255, 255, 255));
+        lblWelc1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons8-currency-96.png"))); // NOI18N
+
+        lblWelc2.setFont(new java.awt.Font("Segoe UI", 0, 10)); // NOI18N
+        lblWelc2.setForeground(new java.awt.Color(255, 255, 255));
+        lblWelc2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblWelc2.setText("IDR | USD | HKD | SGD | EUR");
+
+        lblWelc3.setFont(new java.awt.Font("Segoe UI", 0, 10)); // NOI18N
+        lblWelc3.setForeground(new java.awt.Color(255, 255, 255));
+        lblWelc3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblWelc3.setText("GBP | MYR | KRW | JPY | CNY");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(55, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(lblWelc, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(btnLogin, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(60, 60, 60))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(14, 14, 14)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lblWelc3, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblWelc2, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(57, 57, 57)
+                        .addComponent(lblWelc1))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(28, 28, 28)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(btnLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lblWelc, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(17, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(55, 55, 55)
+                .addGap(12, 12, 12)
+                .addComponent(lblWelc2, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lblWelc1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lblWelc3, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(lblWelc, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 161, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
                 .addComponent(btnLogin)
-                .addGap(35, 35, 35))
+                .addContainerGap(38, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -126,5 +161,8 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JButton btnLogin;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lblWelc;
+    private javax.swing.JLabel lblWelc1;
+    private javax.swing.JLabel lblWelc2;
+    private javax.swing.JLabel lblWelc3;
     // End of variables declaration//GEN-END:variables
 }
