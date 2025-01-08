840c4fa6443fb7bea1ac6d986b4dfa1e295c24d1
DrunkenTapir367 <nabilvidiputra@gmail.com>
2025-01-08 22:03:51 +0700

    capek

--- src/project/konversi/mata/uang/Login.java
+++ src/project/konversi/mata/uang/Login.java
@@ -126,7 +126,7 @@ public class Login extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
+        /* Set the Nimbus look and feels */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
