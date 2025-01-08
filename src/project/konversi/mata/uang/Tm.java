0b636467b75693f2730caac7c1f4e27943bd1d53
DrunkenTapir367 <nabilvidiputra@gmail.com>
2025-01-08 19:19:09 +0700

    prototype1

---
+++ src/project/konversi/mata/uang/Tm.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project.konversi.mata.uang;
+
+/**
+ *
+ * @author Panji
+ */
+public class Tm {
+    double[] dat = {1,16207.32,2085.7,11925.52,16855,20304.04,3625.61,11.06,102.87,2218.99};
+    static String[] nem = new String[10];
+    //public static void nem[0]="hello";
+    
+    
+   public void Dtmain(){
+    Double[] dat= new Double[10];
+    //dat [0] = 1.7;
+    //nem[0]=Double.toString(dat[0]);
+    
+    //for(int i =0;i<10;i++){
+    //nem[i]=Double.toString(dat[i]);}
+    
+    
+    
+}
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login a = new Login();
+        a.setLocationRelativeTo(null);
+        a.setVisible(true);
+        
+    }
+   
+       
+    
+    
+}
+
+
